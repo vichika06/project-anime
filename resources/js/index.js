466fd7b7f6dc28c416ec1372e0b6581886ba5fb7
vichiak06 <ponlokvichika0707@gmail.com>
2024-12-22 00:26:48 +0700

    second commit

--- resources/js/index.js
+++ resources/js/index.js
@@ -11,3 +11,44 @@ function changeImages() {
 }
 
 setInterval(changeImages, 2000);
+
+// Auto-type with libery <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+
+var type = new Typed(".Auto-type", {
+  strings : ["KAI ANIME" , "ខៃ​ អេនីមេ","凯 ANIME","KAI ANIME"],
+  typeSpeed : 150,
+  backSpeed : 150,
+  looped    : true
+})
+
+// nav bar scroll
+
+let lastScroll = 0;
+let navbar = document.querySelector('#scroll-hidden'); 
+
+window.addEventListener('scroll', function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScroll) {
+        navbar.style.top = "-80px"; 
+    } else {
+        navbar.style.top = "0"; 
+    }
+    lastScroll = scrollTop;
+});
+
+
+let lastScrollFooter = 0; 
+let footer = document.querySelector('#scroll-hiddenF');
+window.addEventListener('scroll', function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollFooter) {
+       
+        footer.style.bottom = "-80px"; 
+    } else {
+       
+        footer.style.bottom = "0";
+    }
+
+    lastScrollFooter = scrollTop; 
+});
